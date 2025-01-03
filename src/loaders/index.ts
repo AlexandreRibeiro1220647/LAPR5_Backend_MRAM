04f22a68da1565c133a9db9e19f468413a6ef650
AlexandreRibeiro1220647 <162116947+AlexandreRibeiro1220647@users.noreply.github.com>
2025-01-03 23:07:00 +0000

    Project Cleaning

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -9,18 +9,6 @@ export default async ({ expressApp }) => {
   const mongoConnection = await mongooseLoader();
   Logger.info('✌️ DB loaded and connected!');
 
-  const userSchema = {
-    // compare with the approach followed in repos and services
-    name: 'userSchema',
-    schema: '../persistence/schemas/userSchema',
-  };
-
-  const roleSchema = {
-    // compare with the approach followed in repos and services
-    name: 'roleSchema',
-    schema: '../persistence/schemas/roleSchema',
-  };
-
   const allergySchema = {
     // compare with the approach followed in repos and services
     name: 'allergySchema',
@@ -33,11 +21,6 @@ export default async ({ expressApp }) => {
     schema: '../persistence/schemas/medicalConditionSchema',
   };
 
-  const roleController = {
-    name: config.controllers.role.name,
-    path: config.controllers.role.path
-  }
-
   const allergyController = {
     name: config.controllers.allergy.name,
     path: config.controllers.allergy.path
@@ -46,16 +29,6 @@ export default async ({ expressApp }) => {
   const medicalConditionController = {
     name: config.controllers.medicalCondition.name,
     path: config.controllers.medicalCondition.path
-  }
-
-  const roleRepo = {
-    name: config.repos.role.name,
-    path: config.repos.role.path
-  }
-
-  const userRepo = {
-    name: config.repos.user.name,
-    path: config.repos.user.path
   }
   
   const allergyRepo = {
@@ -66,11 +39,6 @@ export default async ({ expressApp }) => {
   const medicalConditionRepo = {
     name: config.repos.medicalCondition.name,
     path: config.repos.medicalCondition.path
-  }
-
-  const roleService = {
-    name: config.services.role.name,
-    path: config.services.role.path
   }
 
   const allergyService = {
@@ -86,24 +54,18 @@ export default async ({ expressApp }) => {
   await dependencyInjectorLoader({
     mongoConnection,
     schemas: [
-      userSchema,
-      roleSchema,
       allergySchema,
       medicalConditionSchema
     ],
     controllers: [
-      roleController,
       allergyController,
       medicalConditionController
     ],
     repos: [
-      roleRepo,
-      userRepo,
       allergyRepo,
       medicalConditionRepo
     ],
     services: [
-      roleService,
       allergyService,
       medicalConditionService
     ]
